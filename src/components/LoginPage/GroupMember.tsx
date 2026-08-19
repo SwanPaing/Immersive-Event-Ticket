356556c55616de36d19e67336f5b0e4484627636
Your Name <you@example.com>
2026-08-19 22:02:24 +0630

    Group Member Updates

--- src/components/LoginPage/GroupMember.tsx
+++ src/components/LoginPage/GroupMember.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "./GroupMember.css";
 
 const GroupMember = () => {
   return (
     <div className="group-member-container">
       <span className="group-member-title">Created by</span>
-      <span className="group-member-name">Group 1</span>
+      <div className="group-member-name">
+          <span id="s1">Lae Wutt Yee Aung</span>
+          <span id="s2">Phyo Khant Thu Kyaw</span>
+          <span id="s3">Aung Swan Paing</span>
+          <span id="s4">Nay Myo Aung</span>
+          <span id="s5">Aung Moe Myint Thu</span>
+          <span id="s6">Moe Myint Phyu Sin</span>
+          <span id="s7">Nweh Khaing Htae</span>
+          <span id="s8">Aung Thet Lwin</span>
+          <span id="s9">Therapy Thant</span>
+      </div>
     </div>
   );
 };
